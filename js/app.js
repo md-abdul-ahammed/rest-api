7cdc8090389a27c0fbb849788cc9ce9ffadf98fa
md-abdul-ahammed <shagarmd2082@gmail.com>
2021-09-02 18:31:45 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const searchBook = () => {
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    console.log(searchText)
+    searchField.value = '';
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayResult(data.docs))
+}
+const displayResult = books => {
+    const searchResultsCount = document.getElementById('search-result-count')
+    const getResult = books.length
+
+    searchResultsCount.innerHTML = `<p class='container'>We get ${getResult} result</p>`
+    const searchResults = document.getElementById('search-result')
+    searchResults.textContent = ''
+
+    books.forEach(book => {
+        console.log(book)
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = `
+            <div class="card border-info h-100 shadow-sm">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="border-bottom border-primary card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p><i>by <small><b>${book.author_name ? book.author_name[0] : ""}</b></small><i/></p>
+                </div>
+                <div class="card-footer bg-transparent border-primary">
+                <small >Publisher:</br> <b>${book.publisher}</b></small>               
+                </div>
+                <div class="card-footer bg-transparent border-primary">
+                <small>First Publish Year : <b>${book.first_publish_year}</b></small>                
+                </div>
+            </div>
+        `
+        searchResults.appendChild(div)
+    });
+
+}
+
